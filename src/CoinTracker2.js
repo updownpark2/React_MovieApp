bc56a81ae5468dc3726e9e3f261cc75264606678
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-06-30 22:30:44 +0900

    coinTracker
    coin의 가격과 살수있는갯수를 표현하는 프로그램을 만들었다.

--- src/CoinTracker2.js
+++ src/CoinTracker2.js
@@ -3,28 +3,58 @@ import { useEffect, useState } from "react";
 function CoinTracker2() {
   const [loading, setLoading] = useState(true);
   const [coin, setCoin] = useState([]);
+  const [gopage, setGOpage] = useState("");
+  const [money, setMoney] = useState("");
+  const invert = (event) => {
+    setMoney(event.target.value);
+  };
+  const Submit = (event) => {
+    event.preventDefault();
+    <h4>you can buy {money / gopage}</h4>;
+  };
+  const Change = (event) => {
+    setGOpage(event.target.value);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        //이때의 json을 이용한 함수를 실행한다는 개념!
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
+      response.json().then((json) => {
         setCoin(json);
-        setLoading(false); //해야하는 명령이 2가지라서 이렇게 쓴듯?
-      });
+        setLoading(false);
+      })
+    );
   }, []);
-
   return (
+    //gopage는 현재 페이지를 읽을 수 있다.
     <div>
-      {loading ? <strong>loading...</strong> : <h1>hello</h1>}
-      <ul>
-        {coin.map(function (item, index) {
-          return (
-            <li key={index}>
-              {item.name} : {item.quotes.USD.price}
-            </li>
-          );
-        })}
-      </ul>
+      <h2>BTC info</h2>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <select value={gopage} onChange={Change}>
+          {coin.map(function (item, index) {
+            return (
+              <option key={index} value={item.quotes.USD.price}>
+                {item.name}
+              </option>
+            ); //index에따른 item.namm를 렌더링!
+          })}
+        </select>
+      )}
+      <form onSubmit={Submit}>
+        <input
+          onChange={invert}
+          type="number"
+          style={{ margin: "12px 0 0 0 " }}
+          placeholder="Money To Change($)"
+          value={money}
+        />
+        <button style={{ border: 0, borderRadius: 10, margin: "0 0 0 10px" }}>
+          Submit
+        </button>
+      </form>
+
+      <h4>Price: {gopage} $</h4>
+      <h5>You Can buy {money / gopage} 개</h5>
     </div>
   );
 }
